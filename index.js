50ae9fe05b5aa7ac91c11fbab2e3917a7806228c
adryannegorden <adryannegorden@gmail.com>
2023-05-07 23:10:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    if (winningRecord) {
+      return winningRecord.year;
+    }
+    return undefined;
+  }
+  
